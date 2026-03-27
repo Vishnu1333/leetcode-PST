e22ad34f44f7f1321414f04ffd94209dd5750260
Vishnu vardhan <127837652+Vishnu1333@users.noreply.github.com>
2026-03-27 09:56:22 +0530

    Time: 1 ms (99.91%), Space: 44 MB (20.06%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int myAtoi(String s) {
+
+        int i = 0, n = s.length();
+        int sign = 1;
+        long result = 0; // use long to detect overflow
+
+        // 1. Skip whitespaces
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        // 2. Check sign
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            if (s.charAt(i) == '-') {
+                sign = -1;
+            }
+            i++;
+        }
+
+        // 3. Process digits
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            int digit = s.charAt(i) - '0';
+
+            result = result * 10 + digit;
+
+            // 4. Handle overflow
+            if (sign * result > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+            if (sign * result < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+
+            i++;
+        }
+
+        return (int)(sign * result);
+    }
+}
